a79ccd2c8f4faabfa2c78422ea1f0f3ddf36394f
Herbert PC <herbert.quijano@gmail.com>
2024-06-19 15:17:22 -0600

    Added parameterization and corrected bug that was messing with @Parameters annotation

--- src/test/java/org/hquijano/tests/ErrorValidationsTest.java
+++ src/test/java/org/hquijano/tests/ErrorValidationsTest.java
@@ -1,5 +1,6 @@
 package org.hquijano.tests;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.hquijano.pageobjects.CatalogPage;
 import org.hquijano.pageobjects.MyCartPage;
 import org.hquijano.testcomponents.BaseTest;
@@ -9,23 +10,40 @@ import org.testng.annotations.DataProvider;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class ErrorValidationsTest extends BaseTest {
     String url = "https://rahulshettyacademy.com/client";
+    String filepath = System.getProperty("user.dir") + "//src//main//java//org//hquijano//resources//testdata.json";
 
-    @Test(groups = {"ErrorHandling", "LoginTest"}, dataProvider = "loginData")
+    @Test(groups = {"ErrorHandling", "LoginTest"}, dataProvider = "invalidLoginData")
     public void loginErrorValidation(String username, String password) {
         CatalogPage catalogPage = landingPage.login(username, password);
         Assert.assertEquals(landingPage.getErrorMessage(), "Incorrect email or password.", "Expected error message does not match with current message");
     }
 
+    @Test(groups = {"LoginTest"}, dataProvider = "invalidLoginDataMap")
+    public void loginErrorValidationMap(HashMap<String, String> input) {
+        CatalogPage catalogPage = landingPage.login(input.get("user"), input.get("password"));
+        Assert.assertEquals(landingPage.getErrorMessage(), "Incorrect email or password.", "Expected error message does not match with current message");
+    }
+
+    @Test(groups = {"LoginTest"}, dataProvider = "invalidLoginDataJson")
+    public void loginErrorValidationJson(String username, String password) {
+        CatalogPage catalogPage = landingPage.login(username, password);
+        Assert.assertEquals(landingPage.getErrorMessage(), "Incorrect email or password.", "Expected error message does not match with current message");
+    }
+
     @Test(groups = {"Products", "ErrorHandling"})
-    @Parameters({"username", "password", "productToAdd"})
-    public void productErrorValidation(String username, String password, String productToAdd) {
+    @Parameters({"user", "pass", "productToAdd"})
+    public void productErrorValidation(String user, String pass, String productToAdd) {
         String expectedProduct = "ZARA COAT 4";
 
-        CatalogPage catalogPage = landingPage.login(username, password);
+        CatalogPage catalogPage = landingPage.login(user, pass);
         List<WebElement> products = catalogPage.getProductsList();
         catalogPage.getProductToAdd(productToAdd);
         catalogPage.addProductToCart(productToAdd);
@@ -35,11 +53,49 @@ public class ErrorValidationsTest extends BaseTest {
     }
 
     @DataProvider
-    public Object[][] loginData() {
+    public Object[][] invalidLoginData() {
         return new Object[][] {
                 {"ivmora@test.com", "divetteMoral4!"},
                 {"tessalonian22@test.com", "xTess4lon3!"}
         };
 
     }
+
+    @DataProvider
+    public Object[][] invalidLoginDataMap() {
+        HashMap<String, String> user1 = new HashMap<String, String>();
+        user1.put("user","ivmora@test.com");
+        user1.put("password","divetteMoral4!");
+
+        HashMap<String, String> user2 = new HashMap<String, String>();
+        user2.put("user","tessalonian22@test.com");
+        user2.put("password","xTess4lon3!");
+
+
+        return new Object[][] {
+                {user1}, {user2}
+        };
+    }
+
+    @DataProvider
+    public Object[][] invalidLoginDataJson() throws IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        File jsonFile = new File(filepath);
+        List<Map<String, String>> testData = objectMapper.readValue(jsonFile, List.class);
+
+//        for (Map<String, String> s : testData){
+//            System.out.println(s.entrySet());
+//        }
+
+        Object[][] data = new Object[testData.size()][2]; // Two-dimensional array for username and password
+
+        for (int i = 0; i < testData.size(); i++) {
+            Map<String, String> dataMap = testData.get(i);
+            data[i][0] = dataMap.get("username");
+            data[i][1] = dataMap.get("password");
+        }
+
+        return data;
+    }
+
 }
